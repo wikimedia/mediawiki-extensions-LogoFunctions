036cb33a30bd7978a5c47f4d4e4ac05c92781423
Isarra <zhorishna@gmail.com>
2019-05-09 21:15:01 +0000

    Convert to extension registration
    Change-Id: I0761ef1e39402188bbc0566fcc7e87ecf30534ae

--- LogoFunctions.php
+++ LogoFunctions.php
@@ -1,48 +1,14 @@
 <?php
-/**
- * LogoFunctions
- *
- * Adds parser functions relating to the wiki's logo
- *
- * @link https://www.mediawiki.org/wiki/Extension:LogoFunctions
- *
- * @author Devunt <devunt@devunt.kr>
- * @authorlink https://www.mediawiki.org/wiki/User:Devunt
- * @author Ryan Schmidt <skizzerz@gmail.com>
- * @copyright Copyright © 2010 Devunt (Bae June Hyeon) and Ryan Schmidt.
- * @license https://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
- */
-
-// Ensure that the script cannot be executed outside of MediaWiki.
-if ( !defined( 'MEDIAWIKI' ) ) {
-	die( 'This file is a MediaWiki extension and not a valid entry point.' );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'LogoFunctions' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['LogoFunctions'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for the LogoFunctions extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the LogoFunctions extension requires MediaWiki 1.29+' );
 }
-
-// Display extension properties on MediaWiki.
-$wgExtensionCredits['parserhook'][] = [
-	'path' => __FILE__,
-	'name' => 'LogoFunctions',
-	'author' => [
-		'JuneHyeon Bae (devunt)',
-		'Ryan Schmidt',
-		'...'
-	],
-	'url' => 'https://www.mediawiki.org/wiki/Extension:LogoFunctions',
-	'descriptionmsg' => 'logofunctions-desc',
-	'license-name' => 'GPL-2.0-or-later',
-	'version' => '1.1.0'
-];
-
-// config
-// map of namespace name => logo URL
-$wgNamespaceLogos = [];
-
-// Register extension messages and other localisation.
-$wgMessagesDirs['LogoFunctions'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['LogoFunctionsMagic'] = __DIR__ . '/LogoFunctions.i18n.magic.php';
-
-$wgAutoloadClasses['LogoFunctions'] = __DIR__ . '/LogoFunctions.class.php';
-
-// Register extension hooks.
-$wgHooks['ParserFirstCallInit'][] = 'LogoFunctions::onParserFirstCallInit';
-$wgHooks['SkinTemplateOutputPageBeforeExec'][] = 'LogoFunctions::onSkinTemplateOutputPageBeforeExec';
